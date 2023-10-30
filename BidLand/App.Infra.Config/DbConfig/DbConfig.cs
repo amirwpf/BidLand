56aa85069606b766a7c9d60a0177c3580c9b6bc1
amirwpf <hh1900dppp@gmail.com>
2023-10-30 22:38:05 +0330

    add DbConfig

---
+++ BidLand/App.Infra.Config/DbConfig/DbConfig.cs
@@ -0,0 +1,19 @@
+﻿using App.Infra.Db.sqlServer.Ef.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace App.Infra.Config.DbConfig;
+
+public static class DbConfig
+{
+	public static IServiceCollection AddIdentityDbContextService(this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		services.AddDbContext<AppDbContext>(option =>
+			option.UseSqlServer(configuration.GetConnectionString("sqlserver")));
+
+		return services;
+	}
+}
